ca3a4f185d356bf41895282431047a880a7aa7f6
Sara Scaini <sa.scaini@gmail.com>
2019-05-26 21:00:58 +0100

    created App component and flats data

---
+++ src/components/App.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <div className="section-left" />
+        <div className="section-right" />
+      </div>
+    );
+  }
+}
+
+export default App;
